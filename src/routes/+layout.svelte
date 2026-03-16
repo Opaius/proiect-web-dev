739cb8a0a98f47e1dd43c3302a21d851be6be555
cioky <vgromania3@gmail.com>
2026-03-16 22:40:03 +0200

    initial commit
    Co-authored-by: Silviu <silviucorniciuc@gmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+	import './layout.css';
+</script>
+
+<div class="flex max-w-svw gap-2">
+	<div class="w-50 bg-blue-300 p-4">Fetch wether now</div>
+	{@render children?.()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	const url = 'https://api.openweathermap.org/data/2.5/weather';
+
+	const newUrl = new URL(url);
+
+	newUrl.searchParams.append('lat', '47.59827872679939');
+	newUrl.searchParams.append('lon', '25.65846021139883');
+
+	newUrl.searchParams.append('appid', '5a256b5af8fc601315e21019bc92bb7d');
+
+	let data = $state<any | null>(null);
+
+	const fetchData = async () => {
+		const response = await fetch(newUrl);
+		const fetchedData = await response.json();
+		data = fetchedData;
+		
+	};
+	
+</script>
+
+<div class="flex h-svh w-svh flex-col items-center justify-center">
+	<button class="rounded bg-blue-500 p-4 text-white" onclick={fetchData}>Fetch Data</button>
+
+	{#if data}
+		<pre class="text-balanced max-w-full">{JSON.stringify(data, null, 2)}</pre>
+	{:else}
+		<p>No data available</p>
+	{/if}
+</div>
